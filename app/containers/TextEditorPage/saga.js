60c13d72840343354372132735e9f899438dab9b
Prince Christian Basiga <princebasiga@gmail.com>
2018-09-29 17:52:44 -0700

    Implemented saga for updating content and language in DB

---
+++ app/containers/TextEditorPage/saga.js
@@ -0,0 +1,48 @@
+import { takeLatest, put, call} from 'redux-saga/effects';
+
+import { UPDATE_EDITOR_TEXT, LANGUAGE_CHANGED,} from './constants';
+import { updateEditorText } from './actions';
+import firebase from 'firebase';
+
+
+function* updateRoomLanguage(payload){
+
+    const {language, roomId, uid} = payload;
+
+
+    const firestoreRef = firebase.firestore();
+    const roomRef = firestore.collection("CESCollabs").doc("Collabs").collection( uid? uid: "public").doc(roomId);
+
+    //Updates server
+    const response = yield call(roomRef.update({
+        'language': language,
+    }));
+
+}
+
+function* updateRoomText(payload){
+
+    const {newText, roomId, uid} = payload;
+
+    //Then this updates text, might have to be call to server side to use admin privelege.
+    //Nah editing the room's text is public to all.
+
+    const firestore = firebase.firestore();
+    //If no uid provided, then not logged in so not saved one, so public room.
+    const roomRef = firestore.collection("CESCollabs").doc("Collabs").collection( uid? uid: "public").doc(roomId);
+
+
+    //Updates server
+    const response = yield call(roomRef.update({
+        'content': newText,
+    }));
+
+    console.log("response", response);
+}
+
+export default function* saga(){
+
+    yield takeLatest(UPDATE_EDITOR_TEXT, updateRoomText);
+    yield takeLatest(LANGUAGE_CHANGED, updateRoomLanguage);
+
+}
